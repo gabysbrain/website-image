7385b6d08eff2948e5f5023b29e78e83a7b8cbd3
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-07-20 11:23:04 +0100

    add circleci required pkgs

--- default.nix
+++ default.nix
@@ -3,6 +3,14 @@
 pkgs.dockerTools.buildImage {
   name = "gabysbrain-website";
   contents = [ 
+    # needed for circleci
+    pkgs.bashInteractive
+    pkgs.git
+    pkgs.openssh
+    pkgs.gnutar
+    pkgs.gzip
+    pkgs.cacert
+
     pkgs.pandoc 
     pkgs.texlive.combined.scheme-full 
   ];
